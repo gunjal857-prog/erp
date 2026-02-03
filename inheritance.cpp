1465c8ffb8b5084cf39639307a3e5ed56027c07d
gaurav developer <gauravgunjal857@gmail.com>
2026-02-03 14:49:09 +0000

    add inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+namespace student {
+
+class gaurav {
+private:
+    string name = "Gaurav";
+    int age = 21;
+
+public:
+    string course = "Full Stack";
+    int c, cpp, html, css, js;
+
+    gaurav() {
+       
+        cout<<"enter marks for"<<name<<endl<<course<<endl;
+        
+        c   = getMarks("C");
+        cpp = getMarks("C++");
+        html= getMarks("HTML");
+        css = getMarks("CSS");
+        js  = getMarks("JavaScript");
+
+        int total = c + cpp + html + css + js;
+        float average = total / 5.0;
+
+        cout << "\nTotal Marks: " << total << endl;
+        cout << "Average: " << average << endl;
+
+        if (average >= 40)
+            cout << "Status: PASS " << endl;
+        else
+            cout << "Status: FAIL " << endl;
+            showatkt();
+    }
+
+    int getMarks(string subject) {
+        int marks;
+        while (true) {
+            cout << "Enter marks of " << subject << " (0-100): ";
+            cin >> marks;
+
+            if (marks >= 0 && marks <= 100)
+                return marks;
+            else
+                cout << "nvalid marks, try again\n";
+        }
+    }
+   void showatkt() {
+        // bool fail = false;
+
+        // cout << "\nFailed Subjects:\n";
+
+        if (c < 40) {
+            cout << " ATKT in c  : "<<c<<endl;
+            
+        }
+        if (cpp < 40) {
+            cout << "ATKT in cpp :"<<cpp<<endl;
+           
+        }
+        if (html < 40) {
+            cout << "ATKT in html :"<<html<<endl;
+           
+        }
+        if (css < 40) {
+            cout << "ATKT in css :"<<css<<endl;
+         
+        }
+        if (js < 40) {
+            cout << "ATKT in javascript :"<<js<<endl;
+       
+        }
+
+    
+            
+    }
+};
+
+class ruchit:public gaurav{
+  private:
+  string name = "ruchit";
+  int age = 22;
+};
+
+}
+
+
+int main() {
+    using namespace student;
+     ruchit r1; 
+    return 0;
+}
